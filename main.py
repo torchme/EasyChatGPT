1175d126cb9ead1c4ea3c6e53ef41f72a313f919
da-kartushov <kartyshov.d@team.karpov.courses>
2023-06-29 23:30:58 +0300

    make easygpt

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('hello world')
+import streamlit as st
+import gpt4all
+
+if __name__ == "__main__":
+    st.title("Chatbot")
+    with st.spinner("Loading model..."):
+        gptj = gpt4all.GPT4All("orca-mini-3b.ggmlv3.q4_0")
+
+
+    with st.chat_message("EasyChatGPT"):
+        st.write("Hello, how I can help you?")
+
+
+    prompt = st.chat_input('Say something:')
+    if prompt:
+        with st.chat_message("User"):
+            st.write(prompt)
+        response = [{"role": "user", "content": prompt}]
+        answer = gptj.chat_completion(response)
+        answer = answer['choices'][0]['message']['content']
+        with st.chat_message("EasyChatGPT"):
+            st.write(answer)
